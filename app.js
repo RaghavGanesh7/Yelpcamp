4e3c429ef8ae23d0178f0c762a34c3b04a7cb3eb
RaghavGanesh7 <raghavganesh7@gmail.com>
2020-12-30 23:20:14 +0530

    styling

--- app.js
+++ app.js
@@ -2,17 +2,25 @@ const express = require("express");
 const { Mongoose } = require("mongoose");
 const app = express()
 const path = require("path")
+const ejsmate = require('ejs-mate')
 const mongoose = require("mongoose")
 const Campground = require("./models/campground")
 const methodOverride = require("method-override");
 const { title } = require("process");
 
 
-
+app.engine('ejs',ejsmate)
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,'views'))
 app.use(express.urlencoded({extended:true}))
 app.use(methodOverride('_method'));
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js'));
+app.use('', express.static(__dirname + '/node_modules/jquery/dist'));
+
+
+
+
 
 mongoose.connect('mongodb://localhost:27017/yelpcamp',
 {
@@ -20,6 +28,7 @@ mongoose.connect('mongodb://localhost:27017/yelpcamp',
     useCreateIndex:true,
     useUnifiedTopology:true
 });
+
 
 app.get("/campgrounds", async (req,res)=>{
     const campgrounds = await Campground.find({})
--- seeds/index.js
+++ seeds/index.js
@@ -17,13 +17,16 @@ const sample = array => array[Math.floor(Math.random() * array.length)];
 
 const seedDB = async() => {
     await Campground.deleteMany({})
-    for(let i=0;i<300;i++)
+    for(let i=0;i<20;i++)
     {
         const random1000 = Math.floor(Math.random()*1000);
+        const price1 = Math.floor(Math.random()*1000);
         const camp = await new Campground(
             {
                 location: `${cities[random1000].city}, ${cities[random1000].state}`,
                 title: `${sample(descriptors)} ${sample(places)}`,
+                image:'https://images.unsplash.com/photo-1515664952592-0891e0d5f2d0?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
+                price: price1,
                 description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolores vero perferendis laudantium, consequuntur voluptatibus nulla architecto, sit soluta esse iure sed labore ipsam a cum nihil atque molestiae deserunt!',
             }
         ).save()
