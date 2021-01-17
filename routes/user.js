1b1a3722b53ec68933e6a32048262c93620f5635
RaghavGanesh7 <raghavganesh7@gmail.com>
2021-01-17 02:41:45 -0800

    refactoring using router

--- routes/user.js
+++ routes/user.js
@@ -3,13 +3,14 @@ const router = express.Router()
 const passport = require("passport")
 const users = require("../controllers/users")
 
-router.get("/register",users.createUser)
 
-router.post("/register",users.registerUser)
+router.route("/register")
+    .get(users.createUser)
+    .post(users.registerUser)
 
-router.get("/login",users.renderLogin)
-
-router.post("/login",passport.authenticate('local',{failureFlash:true,failureRedirect:"/login"}),users.loginUser)
+router.route("/login")
+    .get(users.renderLogin)
+    .post(passport.authenticate('local',{failureFlash:true,failureRedirect:"/login"}),users.loginUser)
 
 
 router.get("/logout",users.logoutUser)
